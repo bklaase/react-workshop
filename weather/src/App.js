5e9966a758f369e5397ffe39b43575d1e1700411
Bob <psilioxus@haplo.localdomain>
2018-08-10 09:21:22 +0200

    leaflet added to project

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Weather from './weather/weather';
+import Leaflet from './leaflet/Leaflet';
 import './App.css';
-import SettingsContext from './SettingsContext';
-
 
 class App extends Component {
   render() {
     return (
-      // <SettingsContext.Provider value={{controls: false}}>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
               <h1 className="App-title">React openweatherapi exercise</h1>
           </header>
           <Weather />
+          <Leaflet />
         </div>
-      // </SettingsContext.Provider>
     );
   }
 }
